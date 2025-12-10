a3176f29af8ff427e371f35befd15283061aa9a7
HlisTilen <tilen.hlis@proton.me>
2025-12-10 22:49:02 +0100

    feat: naloga 3

--- backend/tests/math.test.js
+++
@@ -1,5 +0,0 @@
-const { add } = require("../src/math");
-
-test("adds numbers", () => {
-  expect(add(2, 3)).toBe(5);
-});
--- backend/tests/server.test.js
+++
@@ -1,8 +0,0 @@
-const request = require("supertest");
-const app = require("../src/server");
-
-test("GET /hello returns JSON", async () => {
-  const res = await request(app).get("/hello");
-  expect(res.status).toBe(200);
-  expect(res.body.message).toBe("Hello Backend");
-});
